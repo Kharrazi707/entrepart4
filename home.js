5708b5b9054e5e39943c6bb80da3b34e7381d464
Kharrazi707 <90320815+Kharrazi707@users.noreply.github.com>
2023-11-24 21:51:20 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+// window.onresize = () => {
+//     width = window.innerWidth;
+//     height = window.innerHeight;
+//     renderer.setSize(width, height);
+//     cam.aspect = width / height;
+
+//     cam.updateProjectionMatrix();
+// };
